b8e58f8cf8acb3288771fc67b623aec18fce74cf
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-03 03:35:39 +0500

    Сгенерированы иодули для api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,6 +4,14 @@ import { getTypeormConfig } from "./config/typeorm.config";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
+import { UsersModule } from './modules/users/users.module';
+import { FavoritesModule } from './modules/favorites/favorites.module';
+import { SubscribeModule } from './modules/subscribe/subscribe.module';
+import { ForumModule } from './modules/forum/forum.module';
+import { FilmsModule } from './modules/films/films.module';
+import { GenreModule } from './modules/genre/genre.module';
+import { NewsModule } from './modules/news/news.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
 	imports: [
@@ -17,7 +25,15 @@ import { join } from "path";
 		}),
 		ServeStaticModule.forRoot({
 			rootPath: join(__dirname, "..", "public")
-		})
+		}),
+		UsersModule,
+		FavoritesModule,
+		SubscribeModule,
+		ForumModule,
+		FilmsModule,
+		GenreModule,
+		NewsModule,
+		AuthModule,
 	],
 })
 export class AppModule {}
