81f3346679eb0e60c0d72ba3b687fdb2125243dc
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-05 23:01:59 +0500

    Сделаны guard и strategy у auth

---
+++ api/src/modules/auth/common/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  city: string;
+}
---
+++ api/src/modules/auth/common/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AuthService } from "../../auth.service";
+import { ConfigService } from "@nestjs/config";
+import { IPayload } from "src/types/jwt.interface";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>("SECRET"),
+      algorithms: ["HS256"]
+    });
+  }
+
+  validate(payload: IPayload) {
+    return await this.authService.validate(payload)
+  }
+}
