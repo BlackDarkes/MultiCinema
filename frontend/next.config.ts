374a1c009ae1e54d9a8088a6aaadb080e5983545
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-11-08 03:28:19 +0500

    Начало работы над client

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,44 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  images: {
+    domains: [process.env.API_HOST!],
+  },
+  trailingSlash: true,
+  async rewrites() {
+    const apiPath = process.env.API_URL || "http://localhost:8000";
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${apiPath}/:path*`,
+      },
+    ];
+  },
+  sassOptions: {
+    additionalData: `
+      @use "src/app/styles/helpers/index.scss" as *;
+      @use "src/app/styles/base/variables.scss" as *;
+    `,
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
